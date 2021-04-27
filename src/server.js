0c2c5c9923e1902e8557602d535753269e91e47f
afieyudha <ayudhatriadi20@gmail.com>
2021-04-27 14:59:33 +0700

    Rev server.js

--- src/server.js
+++ src/server.js
@@ -2,13 +2,23 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
-const init = async () => {
+// localhost
+/* const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host:
-      process.env.NODE_ENV === 'development'
-        ? 'localhost'
-        : 'dicoding-bookshelf.herokuapp.com',
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  }); */
+
+// production
+const init = async () => {
+  const server = Hapi.server({
+    port: process.env.PORT || 5000,
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
